270e52c98c108dd9ac85de297b66a958e7d5d7bc
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2024-02-17 18:01:51 +0100

    AuthSource

--- db/index.js
+++ db/index.js
@@ -2,14 +2,15 @@
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
 
-const password = process.env.password
+const password = process.env.password;
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 //DO NOT TOUCH!!!!
 
 const MONGO_URI =
-  process.env.MONGODB_URI || `mongodb+srv://user:${password}@cluster0.ztdoc7q.mongodb.net/Deployment`;
+  process.env.MONGODB_URI ||
+  `mongodb://user:${password}@ac-du6qg39-shard-00-00.ztdoc7q.mongodb.net:27017,ac-du6qg39-shard-00-01.ztdoc7q.mongodb.net:27017,ac-du6qg39-shard-00-02.ztdoc7q.mongodb.net:27017/?ssl=true&replicaSet=atlas-7n326a-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 mongoose
   .connect(MONGO_URI)
@@ -20,5 +21,3 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
   });
-
-  
